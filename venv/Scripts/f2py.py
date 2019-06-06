19d6008d069e88488c36563bfca53f9f51001b6a
Jonathan Li <jbl@berkeley.edu>
2019-06-06 10:31:03 -0700

    set up lucy's qallse + generated some plots!

---
+++ venv/Scripts/f2py.py
@@ -0,0 +1,4 @@
+#!C:\Users\user\Documents\qpr\venv\Scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'numpy==1.15.4','f2py.py'
+__requires__ = 'numpy==1.15.4'
+__import__('pkg_resources').run_script('numpy==1.15.4', 'f2py.py')
---
+++ venv/Scripts/run_seeding-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\user\Documents\qpr\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'hepqpr-qallse==0.1.0','console_scripts','run_seeding'
+__requires__ = 'hepqpr-qallse==0.1.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('hepqpr-qallse==0.1.0', 'console_scripts', 'run_seeding')()
+    )
